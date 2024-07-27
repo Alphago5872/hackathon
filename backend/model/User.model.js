d6822021ff4c3cbd2c7895b4f814eeb40e16e663
ongsaaa <101408633+ongsaaa@users.noreply.github.com>
2024-07-28 00:54:43 +0700

    backend 1, no db connection or initiation, skeleton model template

---
+++ backend/model/User.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide unique Username"],
+        unique: [true, "Username Exists"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        unique: false,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a unique email"],
+        unique: true,
+    },
+    firstName: { type: String },
+    lastName: { type: String },
+    education: {
+        type: [String],
+    },
+    line: {
+        type: String,
+        required: true
+    },
+    discord: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    interested_topics: {type: [String]},
+    address: { type: String },
+    profile: { type: String }
+});
+
+export default mongoose.model.User || mongoose.model('User', UserSchema);
---
+++ backend/router/route.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+const router = Router();
+
+/** import all controllers */
+import * as controller from '../controllers/appController.js';
+import { registerMail } from '../controllers/mailer.js'
+import Auth, { localVariables } from '../middleware/auth.js';
+
+
+
+/** POST Methods */
+router.route('/register').post(controller.register); // register User
+router.route('/registerMail').post(registerMail); // send the email
+router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate User
+router.route('/login').post(controller.verifyUser,controller.login); // login in app
+
+/** GET Methods */
+router.route('/:username').get(controller.getUser) // get user information
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP) // generate random OTP
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP) // verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession) // reset all the variables
+
+
+/** PUT Methods */
+router.route('/updatuser').put(Auth, controller.updateUser); // is use to update the User profile
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword); // use to reset password
+
+
+
+export default router;
