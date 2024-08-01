c165dd0c47a32bd2a5e7d0df98991a22cef6eb1b
ongsaaa <101408633+ongsaaa@users.noreply.github.com>
2024-08-01 20:23:33 +0700

    cofig.js seen

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export default {
+    JWT_SECRET : "PhNqB+ZCZ3/v2IGVb7F/GAGfgz/xe8tlQqs+x821Cm4=",
+    EMAIL: "hector.schumm14@ethereal.email", // testing email & password
+    PASSWORD : "FJ79aSkZg5TFkvca9j",
+    ATLAS_URI: "mongodb+srv://admin:password1234@empowerhackathon.xcyyiju.mongodb.net/?retryWrites=true&w=majority&appName=empowerhackathon"
+}
+
+// note to self: change all
